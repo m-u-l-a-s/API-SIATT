6bdffcd19b93aedc3e50ac47938bda915b877418
Alícea P. de Lima Rocha <alicealrocha@outlook.com>
2024-04-11 15:25:59 -0300

    :dizzy: style refactor Formulário Online e Presencial

--- frontend/src/pages/FormularioOnline.tsx
+++ frontend/src/pages/FormularioOnline.tsx
@@ -1,3 +1,4 @@
+import ButtonsForms from "../components/ButtonsForms";
 import CalendarPicker from "../components/DateCalendar";
 import TimeChoser from "../components/TimeChoser";
 import { GrAttachment } from "react-icons/gr";
@@ -6,28 +7,34 @@ const FormularioOnline = () => {
 
     return (
 
-        <div className="flex items-start justify-center mt-9 text-black font-medium">
+        <div className="flex flex-col items-start justify-center mt-9 text-black font-medium">
 
-            <form action="" className="flex  justify-center space-x-40">
-                <div className="space-y-5 ">
+            <form action="" className="flex  justify-center space-x-40 ml-36">
+                <div className="space-y-5 ml-20">
 
-                    <div className="flex items-start space-x-4">
-                        <label htmlFor="">Quando:</label>
-                        <CalendarPicker /> {/* CHAMANDO DATA PICKER?  */}
-                        <TimeChoser />
+                    <div className="flex justify-between">
+                        <div className="flex items-start space-x-4">
+                            <label htmlFor="">Data:</label>
+                            <CalendarPicker /> {/* CHAMANDO DATA PICKER?  */}
+                        </div>
+
+                        <div className="ml-4">
+                            <label htmlFor="" className="pr-4">Hora:</label>
+                            <TimeChoser />
+                        </div>
                     </div>
 
                     <div className="flex items-start space-x-2">
 
                         <label>Duração:</label>
-                            <TimeChoser/>
+                        <TimeChoser />
 
                     </div>
 
 
                     <div className="flex items-start space-x-2">
                         <label>Título da Reunião:</label>
-                        <input className="border bg-white border-gray-300 rounded-lg px-3  w-96 h-7 focus:outline-none focus:border-gray-500 focus:ring-gray-400 " type="text" />
+                        <input className="border bg-white border-gray-300 rounded-lg px-3  w-96 h-8 focus:outline-none focus:border-gray-500 focus:ring-gray-400 " type="text" />
                     </div>
 
 
@@ -39,31 +46,18 @@ const FormularioOnline = () => {
                         <textarea className="border bg-white border-gray-300 rounded-lg px-3 py-2 w-full h-20 focus:outline-none focus:border-gray-500 focus:ring-gray-400" />
                     </div>
 
+                </div>
 
+                <div className="space-y-7 ml-10">
+
+                    <div className="flex items-start space-x-2" >
+                        <label htmlFor=""> E-mail dos convidados: </label>
+                        <input placeholder="exemplo@exemplo.com" className="border bg-white border-gray-300 rounded-lg px-3 w-72 h-8 focus:outline-none focus:border-gray-500 focus:ring-gray-400 " type="text"/>
+                    </div>
+                    
                     <div className="flex items-start space-x-2">
                         <label htmlFor="">Anexos:</label>
                         <button><GrAttachment /></button>
-                    </div>
-
-                </div>
-
-                <div className="space-y-5">
-
-                    <div className="flex items-start space-x-2" >
-                        <label htmlFor=""> E-mail dos convidados </label>
-                        <input placeholder="exemplo@exemplo.com" className="border bg-white border-gray-300 rounded-lg px-3  w-96 h-7 focus:outline-none focus:border-gray-500 focus:ring-gray-400 " type="text" />
-                    </div>
-
-
-                    <div className="flex items-start space-x-2">
-                        <label htmlFor="">Número de Convidados:</label>
-                        <select name="cars" id="cars" className="text-center border bg-white border-gray-300 rounded-lg w-72 h-7 focus:outline-none focus:border-gray-500 focus:ring-gray-400">
-                            <option value="volvo"> 1 pessoa </option>
-                            <option value="saab"> Até 4 pessoas</option>
-                            <option value="opel"> Até 10 essoas</option>
-                            <option value="audi"> Até 13 pessoas</option>
-                            <option value="audi"> Até 25 pessoas</option>
-                        </select>
                     </div>
 
 
@@ -73,52 +67,33 @@ const FormularioOnline = () => {
 
                     <div className="flex items-start space-x-2">
                         <label> Sala Online:</label>
-                        <select name="cars" id="cars" className="text-center border bg-white border-gray-300 rounded-lg w-72 h-7 focus:outline-none focus:border-gray-500 focus:ring-gray-400">
-                            <option value="volvo">Sala 1</option>
-                            <option value="saab">Sala 2</option>
-                            <option value="opel">Sala 3</option>
-                            <option value="audi">Sala 4</option>
+                        <select name="salas" id="salas" className="text-center border bg-white border-gray-300 rounded-lg w-72 h-8 focus:outline-none focus:border-gray-500 focus:ring-gray-400">
+                            <option value="sala1">Sala 1</option>
+                            <option value="sala2">Sala 2</option>
+                            <option value="sala3">Sala 3</option>
+                            <option value="sala4">Sala 4</option>
                         </select>
                     </div>
+
                     <div className="flex items-start space-x-2">
                         <label >Sala Presencial:</label>
-                        <select name="cars" id="cars" className="text-center border bg-white border-gray-300 rounded-lg w-72 h-7 focus:outline-none focus:border-gray-500 focus:ring-gray-400">
+                        <select name="cars" id="cars" className="text-center border bg-white border-gray-300 rounded-lg w-72 h-8 focus:outline-none focus:border-gray-500 focus:ring-gray-400">
                             <option className="" value="volvo">Sala 1</option>
                             <option value="saab">Sala 2</option>
                             <option value="opel">Sala 3</option>
                             <option value="audi">Sala 4</option>
                         </select>
-
-
-                    </div>
-
-                    <div className=" flex justify-between">
-                        <button
-                            className="middle none center rounded-lg bg-white border-gray-500 py-4 px-10 font-sans text-xs font-bold uppercase 
-                         text-black shadow-md transition-all hover:shadow-lg
-                         hover:shadow-gray-500 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] 
-                         active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                            data-ripple-light="true"
-                        >Limpar Campos
-                        </button>
-
-                        <button
-                            className="middle none center rounded-lg bg-red-600 py-4 px-12 font-sans text-xs font-bold uppercase 
-                         text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg
-                         hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] 
-                         active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                            data-ripple-light="true"
-                        >
-                            Agendar
-                        </button>
                     </div>
 
                 </div>
             </form>
 
-        </div>
+            <ButtonsForms />
 
+        </div>
+                         
     );
+
 };
 
 export default FormularioOnline;
