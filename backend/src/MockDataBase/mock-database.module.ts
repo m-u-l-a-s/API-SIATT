4a38b661eec8ece0d2c8a58673720212c25ebd6a
Mateus de Sousa <mateus69107@gmail.com>
2024-04-13 16:19:11 -0300

    :card_file_box: Adicionando Reuniões Hibridas e Virtuais

--- backend/src/MockDataBase/mock-database.module.ts
+++ backend/src/MockDataBase/mock-database.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReuniaoEntity } from 'src/reuniao/entities/reuniao.entity';
+import { ReuniaoService } from 'src/reuniao/reuniao.service';
 import { SalaPresencialEntity } from 'src/sala-presencial/entities/sala-presencial.entity';
 import { SalaPresencialService } from 'src/sala-presencial/sala-presencial.service';
 import { SalaVirtualEntity } from 'src/sala-virtual/entities/sala-virtual.entity';
@@ -8,8 +10,8 @@ import { UsuarioEntity } from 'src/usuario/entities/usuario.entity';
 import { UsuarioService } from 'src/usuario/usuario.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UsuarioEntity, SalaPresencialEntity, SalaVirtualEntity])],
-    providers: [SalaPresencialService, UsuarioService, SalaVirtualService],
+    imports: [TypeOrmModule.forFeature([UsuarioEntity, SalaPresencialEntity, SalaVirtualEntity,ReuniaoEntity])],
+    providers: [SalaPresencialService, UsuarioService, SalaVirtualService,ReuniaoService],
     exports: [TypeOrmModule]
 })
 export class MockDataBaseModule {}
